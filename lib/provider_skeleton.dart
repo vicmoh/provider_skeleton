4e7b6bb06cd815c770bf8efed576e49c375393eb
Vicky Mohammad <vimoh1213@gmail.com>
2020-02-25 18:49:00 -0500

    [AUTO]

--- lib/provider_skeleton.dart
+++ lib/provider_skeleton.dart
@@ -1,5 +1,10 @@
-library provider_skeleton;
+library flutter_package_exporter;
 
+export './src/util/util.dart';
+export './src/util/error_result.dart';
+export './src/util/log.dart';
+export './src/util/enum_converter.dart';
+export './src/util/material_util.dart';
 export './src/framework/new_state.dart';
 export './src/framework/view_logic.dart';
 export './src/framework/logics.dart';
