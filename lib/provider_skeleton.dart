841338eaa3d4891122c065f65bb7075b50546be5
Vicky Mohammad <vimoh1213@gmail.com>
2020-02-25 01:23:52 -0500

    mv framework files to framework

--- lib/provider_skeleton.dart
+++ lib/provider_skeleton.dart
@@ -1,13 +1,10 @@
-/// This [file] is used for exporting
-/// the libraries and widget.
 library provider_skeleton;
 
-/// The main libraries to be exported.
-export './src/list_view_logic.dart';
-export './src/logics.dart';
-export './src/model.dart';
-export './src/navigate.dart';
-export './src/new_state.dart';
-export './src/set_state_callback.dart';
-export './src/view_logic.dart';
-export './src/watch_state.dart';
+export './src/framework/new_state.dart';
+export './src/framework/view_logic.dart';
+export './src/framework/logics.dart';
+export './src/framework/set_state_callback.dart';
+export './src/framework/watch_state.dart';
+export './src/framework/navigate.dart';
+export './src/framework/list_view_logic.dart';
+export './src/framework/model.dart';
