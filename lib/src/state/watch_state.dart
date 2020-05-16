9c782704c349c057cb27384b09ffe6ee587c8280
Vicky Mohammad <vimoh1213@gmail.com>
2020-05-15 20:43:57 -0400

    rm context on build

--- lib/src/state/watch_state.dart
+++ lib/src/state/watch_state.dart
@@ -57,7 +57,6 @@ class _WatchStateState<T extends ViewLogic> extends State<WatchState<T>> {
 
   @override
   Widget build(BuildContext context) {
-    _model?.initContext(context);
     return ChangeNotifierProvider<T>.value(
         value: this.widget.logic ?? _model,
         child: Consumer<T>(builder: (context, T model, child) {
