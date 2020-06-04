13e0d9b89d8fd0e59644dca36cf7a267a4ea017b
Vicky Mohammad <vimoh1213@gmail.com>
2020-06-04 08:54:26 -0400

    keep nav record in history

--- lib/src/routing/route_system.dart
+++ lib/src/routing/route_system.dart
@@ -11,23 +11,28 @@ import 'package:flutter/material.dart';
 ///     replaceAsRoot: true, providers: [PostLogic()], page: HomePage());
 /// ```
 abstract class RouteSystem {
+  /// The widget current context.
   final BuildContext context;
+
+  static List<BuildContext> _contexts = [];
 
   /// The recent context used, when navigation
   /// is called, useful for notification handling,
   /// or determining which context you are in.
-  static get recentContext => _recentContext;
-  static BuildContext _recentContext;
+  static BuildContext get recentContext {
+    _contexts = _contexts.where((element) => element != null).toList();
+    return _contexts.last;
+  }
 
   /// A router for navigating to certain pages.
   /// The router needs the [context] of the current state.
   RouteSystem(this.context) {
-    _recentContext = this.context;
+    _contexts.add(context);
   }
 
-  /// Use route system from recent context used.
-  RouteSystem.fromRecentContext() : this.context = _recentContext;
-	
-	/// Go back to previous page.
-	void pop() => Navigator.pop(context);
+  /// Go back to previous page.
+  void pop() {
+    _contexts.removeLast();
+    Navigator.pop(context);
+  }
 }
