ba4bbc188fda2033ef803f5da9a47871d61b8e5c
Vicky Mohammad <vimoh1213@gmail.com>
2020-06-06 17:57:29 -0400

    testing

--- lib/src/state/watch_state.dart
+++ lib/src/state/watch_state.dart
@@ -23,8 +23,11 @@ class WatchState<T extends ViewLogic> extends StatefulWidget {
   /// Get logic from store.
   final String store;
 
-  /// When the logic is ready.
+  /// When the logic is ready. Called in initState.
   final Function(T) onReady;
+
+  /// When the widget is being disposed.
+  final Function(T) onDispose;
 
   /// This is used for watching the state of the [ViewLogic].
   /// Any widgets under the  builder will be watch for refresh,
@@ -32,6 +35,7 @@ class WatchState<T extends ViewLogic> extends StatefulWidget {
   WatchState({
     @required this.builder,
     this.onReady,
+    this.onDispose,
     this.logic,
     this.store,
   }) : assert(builder != null);
@@ -42,6 +46,7 @@ class WatchState<T extends ViewLogic> extends StatefulWidget {
     this.store, {
     @required this.builder,
     this.onReady,
+    this.onDispose,
   })  : assert(builder != null),
         assert(store != null),
         this.logic = null;
@@ -58,16 +63,23 @@ class _WatchStateState<T extends ViewLogic> extends State<WatchState<T>> {
     super.initState();
     if (this.widget.store == null && this.widget.logic == null)
       _model = Logics.getIt<T>();
-    if (this.widget.store != null) _model = Logics.getStore(this.widget.store);
-    if (this.widget.logic != null) _model = this.widget.logic;
+    else if (this.widget.store != null)
+      _model = Logics.getStore(this.widget.store);
+    else if (this.widget.logic != null) _model = this.widget.logic;
     assert(_model != null, 'WatchState(): ViewLogic is not assigned.');
     _model.initContext(context);
     if (this.widget.onReady != null) this.widget.onReady(_model);
   }
 
   @override
+  void dispose() {
+    if (this.widget.onDispose != null) this.widget.onDispose(_model);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) => ChangeNotifierProvider<T>.value(
-      value: this.widget.logic ?? _model,
+      value: _model,
       child: Consumer<T>(builder: (context, T model, child) {
         _model = model;
         _model.initContext(context);
